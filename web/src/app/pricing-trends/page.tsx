763c3e12944adf8499d2124167b07f4298623dde
Andrea Borghi <andreab@compuserve.com>
2026-05-26 14:35:15 -0600

    feat: add Pricing Trends page at /pricing-trends

---
+++ web/src/app/pricing-trends/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { PricingTrends } from "@/components/PricingTrends";
+
+export default function PricingTrendsPage() {
+  return (
+    <div className="space-y-6">
+      <PricingTrends />
+    </div>
+  );
+}
